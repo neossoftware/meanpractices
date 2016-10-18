a43265bdbca61aff6dfaf07fbb32542ff1ea0474
mario <mario.hidalgom@yahoo.com.mx>
2016-10-17 21:50:03 -0500

    node sample

--- chapter2/hellomodule/server.js
+++ chapter2/hellomodule/server.js
@@ -1,4 +1,25 @@
 var hello = require('./hello');
+fs = require('fs');
+
+//this function is part of hello module (hello.js)
 hello.sayHello();
+
+
+fs.readFile('/etc/hosts', 'utf8', function(err, data) {
+  console.log('Testing FS library');
+    if (err) {
+        return console.log(err);
+    }
+    console.log(data);
+});
+
+
+fs.readFile('/etc/shadow', 'utf8', function(err, data) {
+  console.log('Testing Err');
+    if (err) {
+        return console.log(err);
+    }
+    console.log(data);
+});
 
 //invoke node server.js
